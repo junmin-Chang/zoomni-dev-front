5536415928f3bf175d48a8e6da534259e71e6f6e
junmin-chang <chjm219@kookmin.ac.kr>
2021-09-27 18:07:51 +0900

    table style fix

--- components/SidebarData.tsx
+++ components/SidebarData.tsx
@@ -25,6 +25,10 @@ export const sidebarData = [
                 path: '/Redux',
             },
             {
+                title: 'Javascript',
+                path: '/Javascript'
+            },
+            {
                 title: 'Typescript',
                 path: '/Typescript'
             },
